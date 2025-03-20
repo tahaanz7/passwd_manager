c6eed59b250b8e666a07db510436d8acc769c5a8
Taha Anz <tahaanz70@gmail.com>
2025-03-20 20:42:10 +0000

    record card

---
+++ src/components/HomePage.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import PswdCard from "./PswdCard.vue";
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="text-3xl font-bold text-blue-400 text-center mt-20">
+      Password Keeper
+    </div>
+
+    <!-- search bar -->
+    <div class="flex justify-center mt-6 mb-4">
+      <label class="input">
+        <svg
+          class="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input type="search" required placeholder="Search" />
+      </label>
+    </div>
+
+    <div class="flex justify-center mb-25">
+      <button class="btn btn-info">ADD RECORD</button>
+    </div>
+
+    <!-- cards -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 w-max mx-auto gap-4">
+      <PswdCard title="Instagram" login="tahaanz" password="password" />
+      <PswdCard title="Facebook" login="tahaanz" password="password" />
+      <PswdCard title="Google" login="tahaanz" password="password" />
+    </div>
+  </div>
+</template>
---
+++ src/components/PswdCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="border border-gray-300 rounded-xl p-6 w-[300px]">
+    <div class="border-b border-gray-300 font-semibold">{{ title }}</div>
+
+    <!-- login + password -->
+    <div class="mt-6 flex flex-col space-y-2 text-gray-700">
+      <div>
+        <div>Login</div>
+        <div class="border border-gray-200 rounded-lg p-2">{{ login }}</div>
+      </div>
+
+      <div class="flex justify-between border border-gray-200 rounded-lg p-2">
+        <div>{{ password }}</div>
+        <div><FontAwesomeIcon :icon="faHouse" /></div>
+      </div>
+    </div>
+
+    <div class="flex justify-end space-x-2 mt-6 text-sm">
+      <div class="btn btn-error">Delete</div>
+      <div class="btn btn-success">Update</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  login: String,
+  password: String,
+});
+</script>
