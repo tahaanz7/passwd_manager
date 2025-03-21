5c51889ec1b2a3649abe9fe3be6c9ae7f5b9dd76
Mohamed <enahari2000@gmail.com>
2025-03-21 02:37:07 +0000

    Create a Blade Template for Vue App + Update : route/web.php

---
+++ passwd-manager-api/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Manager</title>
+    <!-- If you're using Vite directly in Laravel -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html> 
