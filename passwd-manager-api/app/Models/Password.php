32e64a6984b44dd24d598ebf95ab4ef7495066e7
Mohamed <enahari2000@gmail.com>
2025-03-21 02:09:28 +0000

    Config: Api routes + Updating models: User and Password

--- passwd-manager-api/app/Models/Password.php
+++ passwd-manager-api/app/Models/Password.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Password extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'login',
+        'password',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ passwd-manager-api/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PasswordController;
+use Illuminate\Support\Facades\Route;
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('passwords', PasswordController::class);
+});
